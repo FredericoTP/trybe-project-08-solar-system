c598360790429daf5d6cc3afe3d4b608fa4fc5dd
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-21 15:28:25 -0300

    Requisito 7

---
+++ src/components/Missions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">asd</div>
+    );
+  }
+}
+
+export default Missions;
